7d9a2fce21bfb1f16887dde4bdd96432eb37daba
Ozan Gulle <ozangulle@gmail.com>
2019-03-24 17:39:08 +0100

    Rustup adds target env again

--- ci/before_deploy.sh
+++ ci/before_deploy.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-if [ "$TARGET" == "x86_64-pc-windows-gnu" ]
+if [ "$TARGET" == "x86_64-pc-windows-gnud" ]
 then
     FILENAME=$NAME.exe
 fi
 # Install Rust stdlib for the target
-# rustup target add $TARGET
+rustup target add $TARGET
 
 # Compile the binary for the target
 cargo build --target=$TARGET --release
 
 # Package the release binary
+echo $FILENAME
 tar -C target/$TARGET/release -czf $PACKAGE $FILENAME
