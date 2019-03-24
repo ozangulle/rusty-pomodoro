1934ed1689204adfa6bbed68c931211b849047bc
Ozan Gulle <ozangulle@gmail.com>
2019-03-24 16:43:44 +0100

    Added new deployment script in directory ci

---
+++ ci/before_deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Install Rust stdlib for the target
+rustup target add $TARGET
+
+# Compile the binary for the target
+cargo build --target=$TARGET --release
+
+# Package the release binary
+tar -czf $PACKAGE -C target/$TARGET/release/ $NAME
