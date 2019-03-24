dd7c989afc7a89f8f9615c28e709fbbbaa0c47f4
Ozan Gulle <ozangulle@users.noreply.github.com>
2019-03-24 19:27:53 +0100

    Testing and building on travis ci
    I had to have up on the idea of releasing the binaries on travis for now.

---
+++ ci/before_deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+if [ "$TARGET" == "x86_64-pc-windows-msvc" ]
+then
+    FILENAME=$NAME.exe
+fi
+
+# Install Rust stdlib for the target
+rustup install stable-gnu
+rustup target add $TARGET
+
+# Compile the binary for the target
+cargo build --target=$TARGET --release
+
+# Package the release binary
+tar -C target/$TARGET/release -czf $PACKAGE $FILENAME
