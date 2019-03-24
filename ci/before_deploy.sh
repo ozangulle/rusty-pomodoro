cbc625876b76207981c6566d16bea503520689ea
Ozan Gulle <ozangulle@gmail.com>
2019-03-24 18:12:02 +0100

    Trying to compile only for windows now

--- ci/before_deploy.sh
+++ ci/before_deploy.sh
@@ -3,12 +3,13 @@ if [ "$TARGET" == "x86_64-pc-windows-gnud" ]
 then
     FILENAME=$NAME.exe
 fi
+
 # Install Rust stdlib for the target
+rustup install stable-gnu
 rustup target add $TARGET
 
 # Compile the binary for the target
 cargo build --target=$TARGET --release
 
 # Package the release binary
-echo $FILENAME
 tar -C target/$TARGET/release -czf $PACKAGE $FILENAME
